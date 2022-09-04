d90e3e0713c10487f3fa4f4eeecd5657b142c666
Tim Ganter <1911000+timganter@users.noreply.github.com>
2022-09-04 10:58:15 -0700

    Update all packages and make appropriate tweaks after upgrade.

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,16 +1,20 @@
 // == External
 import React, { FC } from 'react'
-import { observer }  from 'mobx-react-lite'
+import { observer } from 'mobx-react-lite'
 
 // == App
 import { useRootStore } from './store/RootStore'
 
-export const Counter:FC = observer(() => {
+export const Counter: FC = observer(() => {
   const store = useRootStore()
 
-  return <>
-    <button onClick={() => store.countIncrement()}>Increment counter</button>
-    <button onClick={() => store.countDecrement()}>Decrement counter</button>
-    <strong>{store.count}</strong>
-  </>
+  return (
+    <>
+      <button onClick={() => store.countIncrement()}>Increment counter</button>
+      <button onClick={() => store.countDecrement()}>Decrement counter</button>
+      <p>
+        <strong>{store.count}</strong>
+      </p>
+    </>
+  )
 })
--- src/Fox.tsx
+++ src/Fox.tsx
@@ -1,27 +1,31 @@
 // == External
 import React, { FC, useEffect } from 'react'
-import { observer }  from 'mobx-react-lite'
+import { observer } from 'mobx-react-lite'
 
 // == App
 import { useRootStore } from './store/RootStore'
 
-export const Fox:FC = observer(() => {
+export const Fox: FC = observer(() => {
   const store = useRootStore()
 
   useEffect(() => {
     store.fetchAFox()
   }, [store])
 
-  return <div>
-    <h2>{store.hasFox() ? <FoxButton /> : 'Fetching a fox...'}</h2>
-    {store.hasFox() && <img src={store.fox.image} width="400" />}
-  </div>
+  return (
+    <div>
+      <h2>{store.hasFox() ? <FoxButton /> : 'Fetching a fox...'}</h2>
+      {store.hasFox() && <img src={store.fox.image} width="400" />}
+    </div>
+  )
 })
 
-const FoxButton:FC = observer(() => {
+const FoxButton: FC = observer(() => {
   const store = useRootStore()
-  return <>
-    Here's a fox!
-    <button onClick={() => store.fetchAFox()}>Fetch another fox</button>
-  </>
+  return (
+    <>
+      <h3>Here is a fox!</h3>
+      <button onClick={() => store.fetchAFox()}>Fetch another fox</button>
+    </>
+  )
 })
